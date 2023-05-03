5593cc93173027852f7faa5886c74dd892b97999
aripddev <dev@aripd.com>
2023-05-03 21:10:07 +0200

    Initial Commit

---
+++ MacTile/MacTileApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacTileApp.swift
+//  MacTile
+//
+//  Created by Cem Selman on 3.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct MacTileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
